b293e73706bc2534ce9722cc818ebc02cff34957
Galih Pratama <hanamura.iost@gmail.com>
2021-01-17 23:24:48 +0700

    Model and migration

---
+++ database/migrations/2021_01_17_161441_create_transactions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('users_id');
+
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->text('address')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->string('courier')->nullable();
+            
+            $table->string('payment')->default('MIDTRANS');
+            $table->string('payment_url')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
