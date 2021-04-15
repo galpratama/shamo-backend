1221318bf9828ca193ec6e4ab73d93596c8e6203
Galih Pratama <hanamura.iost@gmail.com>
2021-04-15 23:51:47 +0700

    Update url function at cms

--- app/Http/Controllers/ProductGalleryController.php
+++ app/Http/Controllers/ProductGalleryController.php
@@ -32,7 +32,7 @@ class ProductGalleryController extends Controller
                         </form>';
                 })
                 ->editColumn('url', function ($item) {
-                    return '<img style="max-width: 150px;" src="'. Storage::url($item->url) .'"/>';
+                    return '<img style="max-width: 150px;" src="'. $item->url .'"/>';
                 })
                 ->editColumn('is_featured', function ($item) {
                     return $item->is_featured ? 'Yes' : 'No';
