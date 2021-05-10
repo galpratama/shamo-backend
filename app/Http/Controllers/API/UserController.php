3c06a7099d74f55be7ce6b40657dc4ecf5ae1977
Galih Pratama <hanamura.iost@gmail.com>
2021-05-11 00:38:29 +0700

    Add phone

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -75,6 +75,7 @@ class UserController extends Controller
                 'name' => ['required', 'string', 'max:255'],
                 'username' => ['required', 'string', 'max:255', 'unique:users'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'phone' => ['nullable', 'string', 'max:255'],
                 'password' => ['required', 'string', new Password]
             ]);
 
@@ -82,6 +83,7 @@ class UserController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
                 'username' => $request->username,
+                'phone' => $request->phone,
                 'password' => Hash::make($request->password),
             ]);
 
